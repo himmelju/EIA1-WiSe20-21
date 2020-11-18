885da7cb67c1a7f2b730dc47b2084c3f9f1de4b1
himmelju <julian.himmel@t-online.de>
2020-11-18 11:32:58 +0100

    Berechnungen

--- Aufgaben/Aufgabe05/script.js
+++ Aufgaben/Aufgabe05/script.js
@@ -1,10 +1,10 @@
 //Emissionswerte der Regionen 2008 und 2018
-//Afrika
+//Africa
 var africa2008 = 1028;
 var africa2018 = 1235.5;
 //South America
 var southAmerica2008 = 1132.6;
-var southamerica2018 = 1261.5;
+var southAmerica2018 = 1261.5;
 //Europe
 var europe2008 = 4965.7;
 var europe2018 = 4209.3;
@@ -17,4 +17,25 @@ var asia2018 = 16274.1;
 //Australia
 var australia2008 = 1993;
 var australia2018 = 2100.5;
+//Emissionswerte der Regionen
+//Berechnungen 
+//Gesamtemissionen
+var gesEmission = africa2018 + southAmerica2018 + europe2018 + northAmerica2018 + asia2018 + australia2018;
+//Africa
+var prozentAfricaGes = ((africa2018 / gesEmission) * 100).toFixed(2);
+var wachstumAfricaP = (((africa2018 / africa2008) - 1) * 100).toFixed(2);
+var wachstumAfricaKG = (africa2018 - africa2008).toFixed(2);
+//South America
+var prozentSouthAmericaGes = ((southAmerica2018 / southAmerica2008) * 100).toFixed(2);
+var wachstumSouthAmericaP = (((southAmerica2018 / southAmerica2008) - 1) * 100).toFixed(2);
+var wachstumSouthAmericaKG = (southAmerica2018 - southAmerica2008).toFixed(2);
+//Europe
+var prozentEuropeGes = ((europe2018 / europe2008) * 100).toFixed(2);
+var wachstumEuropeP = (((europe2018 / europe2008) - 1) * 100).toFixed(2);
+var wachstumEuropeKG = (europe2018 - europe2008).toFixed(2);
+//North America
+var prozentNorthAmericaGes = ((northAmerica2018 / northAmerica2008) * 100).toFixed(2);
+var wachstumNorthAmericaP = (((northAmerica2018 / northAmerica2008) - 1) * 100).toFixed(2);
+var wachstumNorthAmericaKG = (northAmerica2018 - northAmerica2008).toFixed(2);
+//Asia
 //# sourceMappingURL=script.js.map
