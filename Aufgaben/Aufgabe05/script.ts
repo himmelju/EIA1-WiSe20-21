fa2c6c43ffad1f949e7b582f3a575d92a728a4d0
himmelju <julian.himmel@t-online.de>
2020-11-18 10:56:07 +0100

    Varibalen

---
+++ Aufgaben/Aufgabe05/script.ts
@@ -0,0 +1,19 @@
+//Emissionswerte der Regionen 2008 und 2018
+//Afrika
+var africa2008 = 1028;
+var africa2018 = 1235.5;
+//South America
+var southAmerica2008 = 1132.6;
+var southamerica2018 = 1261.5;
+//Europe
+var europe2008 = 4965.7;
+var europe2018 = 4209.3;
+//North America
+var northAmerica2008 = 6600.4;
+var northAmerica2018 = 6035.6;
+//Asia
+var asia2008 = 12954.7;
+var asia2018 = 16274.1;
+//Australia
+var australia2008 = 1993;
+var australia2018 = 2100.5;
