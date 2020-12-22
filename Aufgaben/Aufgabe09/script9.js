c069d4484fa32d88784f8cdb94e7dbe81acf9f71
himmelju <julian.himmel@t-online.de>
2020-12-22 21:50:00 +0100

    keypress

--- Aufgaben/Aufgabe09/script9.js
+++ Aufgaben/Aufgabe09/script9.js
@@ -3,42 +3,15 @@ var NewTask = document.getElementById("NewTask");
 //Variable Aufgaben zu erledigen übrig
 var number = 0;
 //Funktion der Entertaste
-document.querySelector("NewTask").addEventListener("keypress", function (event) {
-    if (event.key == "Enter") {
+NewTask.addEventListener("keypress", function (e) {
+    if (e.key == "Enter") {
         addNewTask();
+        deleteAll();
         NewTask.value = "";
     }
 });
 //Funktion neue Aufgabe hinzufügen und Variablen deklarieren
 function addNewTask(task) {
-    var mybox = document.createElement("mybox");
-    mybox.className = "mybox";
-    var checkbox = document.createElement("checkbox");
-    checkbox.className = "checkClick";
-    checkbox.type = "checkbox";
-    var text = document.createElement("Text");
-    text.className = "NewTaskText";
-    text.innerHTML = NewTask.value;
-    var trash = document.createElement("trash");
-    trash.className = "fas fa-trash-alt";
-    //Funktion Mülleimer Klicken
-    trash.addEventListener("click", function () {
-        removeTask(mybox);
-    });
-    document.getElementById("NewTask").appendChild(mybox);
-    mybox.appendChild(checkbox);
-    mybox.appendChild(text);
-    mybox.appendChild(trash);
-    checkbox.addEventListener("click", function () {
-        if (checkbox.classList.contains("done")) {
-            checkbox.classList.remove("done");
-            text.classList.remove("line-through");
-        }
-        else {
-            checkbox.classList.add("done");
-            text.classList.add("crossedout");
-        }
-    });
     number++;
     updateNumber();
 }
