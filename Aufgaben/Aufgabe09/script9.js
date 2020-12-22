c0eb87b10d6051228af435b7cd9188c11d575ee2
himmelju <julian.himmel@t-online.de>
2020-12-22 21:32:34 +0100

    script

--- Aufgaben/Aufgabe09/script9.js
+++ Aufgaben/Aufgabe09/script9.js
@@ -1,71 +1,55 @@
-// Jedes Event auf der Seite neu laden
-loadEvents();
-// Die Funktion submit wird aufgerufen - Absenden der ToDo`s wird möglich gemacht!
-// Die Funktion clearList wird bei Klick aufgerufen - Die gesamte Liste mit allen ToDo`s wird gelöscht! 
-function loadEvents() {
-    document.querySelector("form").addEventListener("submit", submit);
-    document.getElementById("clear").addEventListener("click", clearList);
-}
-// Funktion: Absenden
-var input = document.querySelector("input");
-function submit(e) {
-    e.preventDefault(); // Javascript Event preventDefault übernimmt im DOM die Aufgabe des Aufrufs return false;, um die ursprüngliche Aktion des Browsers bei einem HTML-Element außer Kraft zu setzen.
-    if (input.value != " ")
-        addTask(input.value);
-    input.value = " "; // Das Textfeld wird wieder leer!
-}
-// Tasks hinzufügen (let - Variablen in die Funktion rein (sollen nur in dem Bereich gültig sein - im Deklarationsbereich - Bei addTask-Funktion also erst erscheinen sollen)
-//Variablen
-var doTodoElements = document.getElementsByClassName("doTodo");
-var deleteTodoElements = document.getElementsByClassName("delete");
-var taskboard = document.querySelector(".tasksBoard");
-//Funktion
-function addTask(task) {
-    var ul = document.querySelector("ul");
-    var li = document.createElement("li");
-    if (taskboard.classList.contains("is-hidden"))
-        taskboard.classList.remove("is-hidden"); // Aufgabenboard erscheint
-    li.innerHTML = "<span class=\"delete\"><i class=\"fas fa-trash-alt\"></i></span><input class=\"doTodo\" type=\"checkbox\"><label>" + task + "</label>"; // Mit dem Dollerzeichen bestimme ich, dass sich die Checkbox ausschließlich auf dieses Label bezieht (Ist dann Javascript zwischen den geschweiften Klammern). Checkbox hat immer ein Label (In dem Fall Inhalt meiner Todo) Tamplate Literals -> durch ` ` kann ich HTML dazwischen schreiben
-    ul.appendChild(li); // Damit immer ein neues hinzugefügt werden kann!
-    for (var i = 0; i < doTodoElements.length; i++) {
-        doTodoElements[i].addEventListener("click", tickTask, false);
+//Variable neue Aufgabe Eingabefeld
+var NewTask = document.getElementById("NewTask");
+//Variable Aufgaben zu erledigen übrig
+var number = 0;
+//Funktion der Entertaste
+document.querySelector("NewTask").addEventListener("keypress", function (event) {
+    if (event.key == "Enter") {
+        addNewTask();
+        NewTask.value = "";
     }
-    for (var i = 0; i < doTodoElements.length; i++) {
-        deleteTodoElements[i].addEventListener("click", deleteTask, false); // delete Task wird nur durch Klich ausgeführt
-    }
-    numberoflistelements();
+});
+//Funktion neue Aufgabe hinzufügen und Variablen deklarieren
+function addNewTask(task) {
+    var mybox = document.createElement("mybox");
+    mybox.className = "mybox";
+    var checkbox = document.createElement("checkbox");
+    checkbox.className = "checkClick";
+    checkbox.type = "checkbox";
+    var text = document.createElement("Text");
+    text.className = "NewTaskText";
+    text.innerHTML = NewTask.value;
+    var trash = document.createElement("trash");
+    trash.className = "fas fa-trash-alt";
+    //Funktion Mülleimer Klicken
+    trash.addEventListener("click", function () {
+        removeTask(mybox);
+    });
+    document.getElementById("NewTask").appendChild(mybox);
+    mybox.appendChild(checkbox);
+    mybox.appendChild(text);
+    mybox.appendChild(trash);
+    checkbox.addEventListener("click", function () {
+        if (checkbox.classList.contains("done")) {
+            checkbox.classList.remove("done");
+            text.classList.remove("line-through");
+        }
+        else {
+            checkbox.classList.add("done");
+            text.classList.add("crossedout");
+        }
+    });
+    number++;
+    updateNumber();
 }
-// Anzeigen, wie viele todos noch existieren
-function numberoflistelements() {
-    document.getElementById("number").innerHTML = "elements left: " + doTodoElements.length;
+//Funktion Menge der Aufagben die noch zu erledigen sind anzuzeigen
+function updateNumber() {
+    document.getElementById("TasksLeft").innerHTML = number + "Aufgaben übrig";
 }
-// ToDo löschen - // DOM - Manipulation durch Document.adoptNode()!
-// parentNode und nextSilbling greifen auf Elemente zu, 
-// die nicht direkt durch Methoden wie getElementById, getElementsByName 
-// oder querySelector erreicht werden und finden jedes Element im DOM auch ohne ID und class-Attribut.
-// Node = Schnittstelle -> Typen werden vererbt.
-function deleteTask() {
-    var remove = this.parentNode;
-    var parentNode = remove.parentNode;
-    parentNode.removeChild(remove); // Das nächste Kind wird entfernt
-    numberoflistelements();
-}
-// ToDo ankreuzen
-function tickTask(e) {
-    var task = e.target.nextSibling;
-    if (e.target.checked) { // checked --> Checkbox ist angeklickt/Aufgabe wurde erledigt
-        task.style.textDecoration = "line-through"; // ToDo wird durchgestrichen. Ist das okay, wenn ich es hier mache? Oder mit CSS-Klassen?
-        task.style.color = "#ff0000";
-    }
-    else {
-        task.style.textDecoration = "";
-        task.style.color = "#2f4f4f";
-    }
-}
-// Löschen der gesamten Liste
-var ul = document.querySelector("ul");
-function clearList() {
-    ul.innerHTML = " ";
+//delete All Button Funktion
+function deleteAll() {
+    this.parentElement.remove();
+    updateNumber();
 }
 //# sourceMappingURL=TypeScript.js.map
 //# sourceMappingURL=script9.js.map
